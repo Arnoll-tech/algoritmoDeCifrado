3ea785b15013ad1e86b5893ba87b114551619779
had3s <had3s@kali.had3s>
2024-05-01 17:00:39 -0500

    1.0.1 se estructura la primera funcion de integracion

--- src/cifrado.py
+++ src/cifrado.py
@@ -7,3 +7,8 @@ sum_clave_y_cesar = 0
 texto_en_cesar = ""
 texto_en_vigenere = ""
 texto_en_hex = ""
+
+def integracionCesar(texto_inicial,indice_de_rotacion,texto_en_cesar):
+
+    if (indice_de_rotacion >= 1 & indice_de_rotacion <= 26):
+        
