fd2be5dbf9f26330014704dfd9c6cf781708b1ff
had3s <had3s@kali.had3s>
2024-05-01 19:31:28 -0500

    1.0.2 se reestructura la funcion de integracion cesar

--- src/cifrado.py
+++ src/cifrado.py
@@ -1,4 +1,4 @@
-import modulos 
+from modulos.mod_tablas import iteracion_de_tabla
 
 texto_inicial = ""
 indice_de_rotacion = 0
@@ -7,8 +7,17 @@ sum_clave_y_cesar = 0
 texto_en_cesar = ""
 texto_en_vigenere = ""
 texto_en_hex = ""
+entrada_texto = "Ingresa texto a cifrar"
+entrada_indice = "Ingresa el numero de rotacion"
 
-def integracionCesar(texto_inicial,indice_de_rotacion,texto_en_cesar):
+def integracionCesar():
+    texto_inicial = input(print(entrada_texto))
+    indice_de_rotacion = int(input(print(entrada_indice)))
+    if (indice_de_rotacion >= 1 and indice_de_rotacion <= 26 ):
+        iteracion_de_tabla(texto_inicial, indice_de_rotacion, texto_en_cesar)
 
-    if (indice_de_rotacion >= 1 & indice_de_rotacion <= 26):
-        
+        print("Ok")
+    else:
+        print("Ingresa un numero valido")
+
+integracionCesar()
