e4c3ae8bb4d925d72e0560e8d0e50af3299ccc2d
had3s <had3s@kali.had3s>
2024-05-05 10:01:02 -0500

    se crea la funcion vigenere

--- src/cifrado.py
+++ src/cifrado.py
@@ -1,24 +1,30 @@
-##from modulos import mod_tablas
+#from modulos import mod_tablas
 
 texto_inicial = ""
 indice_de_rotacion = 0
-palabra_clave = ""
+palabra_clave = "Ingresa la palabra clave:"
 sum_clave_y_cesar = 0
 texto_en_cesar = ""
 texto_en_vigenere = ""
 texto_en_hex = ""
-entrada_texto = "Ingresa texto a cifrar"
-entrada_indice = "Ingresa el numero de rotacion"
-alfabeto = "abcdefghijklmnopeqrstuvwxyz"
+entrada_texto = "Ingresa texto a cifrar:"
+entrada_indice = "Ingresa el numero de rotacion:"
+alfabeto = "abcdefghijklmnopqrstuvwxyz"
+espacios = " "
 
 def integracionCesar():
-    texto_inicial = input(print(entrada_texto))
-    indice_de_rotacion = int(input(print(entrada_indice)))
+    texto_inicial = input(entrada_texto + espacios)
+    indice_de_rotacion = int(input(entrada_indice + espacios))
     if (indice_de_rotacion >= 1 and indice_de_rotacion <= 26 ):
             rotacion = alfabeto[+indice_de_rotacion:] + alfabeto[:indice_de_rotacion]
             traduccion = str.maketrans(alfabeto + alfabeto.upper(), rotacion + rotacion.upper())
             texto_en_cesar = texto_inicial.translate(traduccion)
-            print(texto_en_cesar)
+            print(texto_en_cesar)   
     else:
         print("Ingresa un numero valido")
 integracionCesar()
+
+
+def integracionVigenere(texto_en_cesar):
+     palabra_clave = str(input(palabra_clave + espacios))
+     str.maketrans(texto_en_cesar + texto_en_cesar.punctuation)
