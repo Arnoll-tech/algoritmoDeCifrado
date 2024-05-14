bb6543e41cb27326d1c234c20180daed61e31e97
had3s <had3s@kali.had3s>
2024-05-14 12:31:37 -0500

    1.0.12 se crean las funciones para pasar como argumentos help y verbose

--- src/modulos/mod_argumentos.py
+++ src/modulos/mod_argumentos.py
@@ -1,32 +1,27 @@
 import argparse
 
-
 error = "Error: comando no encontrado"
 
 #La funcion muestra el procesos de cifrado a medida que se ejecuta el script con el parametro verbose
-def verbose():
-    string = argparse.ArgumentParser(description="cryptiverse [-v][--v][-verbose][--verbose]")
-    string.add_argument("-v","--v","-verbose","--verbose", type=str, help="Muestra en pantalla el cifrado a medida que se le pasan los parametros")
+def verbose(parametro_cifrado):
+    string = argparse.ArgumentParser(description="crptiverse [-v][--v][-verbose][--verbose]")
+    string.add_argument("-v", "--v", "-verbose", "--verbose", action="store_true", help="Muestra en pantalla el cifrado a medida que se le pasan los parametros")
     argumentos = string.parse_args()
 
-    if argumentos.verbose:
-        print("ok")
-    else:
-        print("error")
+    if argumentos.v:
+        print(parametro_cifrado)
 
-if __name__ == "__verbose__":
-    verbose()
+    if __name__ == "__verbose__":
+        verbose(parametro_cifrado)
 
 #La funcion despliega un menu de ayuda del script
 def help():
-    string = argparse.ArgumentParser(description="cryptiverse [-h][--h][-help][--help]")
-    string.add_argument("-h", "--h", "-help", "--help", type=str, help="Muestra los comandos que se pueden usar para el cifrado")
-    argumentos = string.parse_args()
+    cadena = argparse.ArgumentParser(description="cryptiverse [-h][--h][-help][--help]")
+    cadena.add_argument("-h", "--h", "-help", "--help", action="store_true", help="Muestra los comandos que se pueden usar para el cifrado")
+    argument = cadena.parse_args()
 
-    if argumentos.help:
-        verbose()
-    else:
-        print(error)
-        
+    if argument.help:
+        cadena.print_help()
+        exit()
 if __name__=="__help__":
     help()
