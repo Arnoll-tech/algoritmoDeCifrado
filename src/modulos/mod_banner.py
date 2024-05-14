19ff49a4275e70b5f4cf2e79dc8cc14851c99306
had3s <had3s@kali.had3s>
2024-05-13 22:41:28 -0500

    1.0.10 se le agrego detalles al banner

--- src/modulos/mod_banner.py
+++ src/modulos/mod_banner.py
@@ -9,6 +9,8 @@ def banner():
 
     bann = pyfiglet.figlet_format(nombre,font=estilo)
     bann_color = color + bann + Fore.RESET
+    print("*" * 65)
     print(bann_color)
+    print("*" * 65)
 if __name__=="__banner__":
     banner()
