5bda8e02af0db8b480be9c23cf983f8a7ad9540b
Amadeo <abec2003@hotmail.com>
2024-05-25 17:51:36 -0400

    Add files via upload
    Rama de una interfaz grafica para el programa CryptiVerse

---
+++ src/utils/constants.py
@@ -0,0 +1,12 @@
+"""
+    Aqui se construye las mayorias de constantes 
+    que van a ser usadas en el programa
+"""
+
+TITLE: str ="CryptiVerse"
+THEMENAME: str ="united"
+WINDOW_SIZE: tuple =(480, 280)
+WINDOW_MINSIZE: tuple =(380,380)
+
+
+
