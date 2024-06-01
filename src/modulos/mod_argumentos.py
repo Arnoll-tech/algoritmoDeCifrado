68e37d517060c5704615c1ef5555f92398198fa9
had3s <had3s@kali.had3s>
2024-05-31 21:00:19 -0500

    modificacion de la funcion  main

--- src/modulos/mod_argumentos.py
+++ src/modulos/mod_argumentos.py
@@ -1,27 +1,16 @@
 import argparse
-
 error = "Error: comando no encontrado"
 
 #La funcion muestra el procesos de cifrado a medida que se ejecuta el script con el parametro verbose
-def verbose(parametro_cifrado):
-    string = argparse.ArgumentParser(description="crptiverse [-v][--v][-verbose][--verbose]")
-    string.add_argument("-v", "--v", "-verbose", "--verbose", action="store_true", help="Muestra en pantalla el cifrado a medida que se le pasan los parametros")
+def main(text):
+    string = argparse.ArgumentParser(description="cifrado.py [-v][--verbose], [-h][--help]")
+    string.add_argument("-v","--verbose", action="store_true", help="Muestra en pantalla el cifrado a medida que se le pasan los parametros")
+    
     argumentos = string.parse_args()
 
-    if argumentos.v:
-        print(parametro_cifrado)
-
-    if __name__ == "__verbose__":
-        verbose(parametro_cifrado)
-
-#La funcion despliega un menu de ayuda del script
-def help():
-    cadena = argparse.ArgumentParser(description="cryptiverse [-h][--h][-help][--help]")
-    cadena.add_argument("-h", "--h", "-help", "--help", action="store_true", help="Muestra los comandos que se pueden usar para el cifrado")
-    argument = cadena.parse_args()
-
-    if argument.help:
-        cadena.print_help()
-        exit()
-if __name__=="__help__":
-    help()
+    if argumentos.verbose:
+        print(text)
+        
+    if __name__ == "__main__":
+        main()
+        
